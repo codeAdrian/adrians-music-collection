e8d3c63f3c2ab884eaa7e2985e921d0d84e9a309
Adrian Bece <adrian.bece@inchoo.net>
2019-05-21 15:22:01 +0200

    Moving firebase credentials .env

--- src/modules/firebase/firebase.ts
+++ src/modules/firebase/firebase.ts
@@ -6,13 +6,13 @@ import 'firebase/firestore';
 import { FirebaseOptions } from '@firebase/app-types';
 
 const FIREBASE_CONFIG: FirebaseOptions = {
-    apiKey: 'AIzaSyDwHBLX8P5RbZYtAFqgzC6CJOIunA3d0RQ',
-    authDomain: 'adrian-s-music-collectio-14bdb.firebaseapp.com',
-    databaseURL: 'https://adrian-s-music-collectio-14bdb.firebaseio.com',
-    projectId: 'adrian-s-music-collectio-14bdb',
-    storageBucket: 'adrian-s-music-collectio-14bdb.appspot.com',
-    messagingSenderId: '108526308076',
-    appId: '1:108526308076:web:c59f11e8036abd6d'
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 export const firebaseInit = firebase.initializeApp(FIREBASE_CONFIG);
