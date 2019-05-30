f5bab60cee1feef88635303c519b5cc115aa46ab
Adrian Bece <adrian@prototyp.digital>
2019-05-30 18:12:32 +0200

    Responsive styles

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,20 +1,20 @@
-import React from 'react';
-import { InputProps } from 'models';
+import React from "react";
+import { InputProps } from "models";
 
 const Input: React.FC<InputProps> = ({
-    label,
-    meta,
-    hideLabel,
-    className,
-    ...rest
+  label,
+  meta,
+  hideLabel,
+  className,
+  ...rest
 }) => (
-    <div className='input'>
-        <label className={hideLabel ? 'hidden' : 'input__label'}>{label}</label>
-        <div className={`${className ? className : 'input__wrapper'}`}>
-            <input className='input__element' {...rest} />
-        </div>
-        {!hideLabel && meta.touched && meta.error && <span>{meta.error}</span>}
+  <div className="input">
+    <label className={hideLabel ? "hidden" : "input__label"}>{label}</label>
+    <div className={`${className ? className : "input__wrapper"}`}>
+      <input autoComplete="off" className="input__element" {...rest} />
     </div>
+    {!hideLabel && meta.touched && meta.error && <span>{meta.error}</span>}
+  </div>
 );
 
 export { Input };
